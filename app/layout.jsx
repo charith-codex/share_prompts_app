457b12bdd8da181d808ffea8b5aa33d956be459b
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-03-01 13:46:00 +0530

    version 1.0

---
+++ app/layout.jsx
@@ -0,0 +1,29 @@
+import '@styles/globals.css'
+
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+
+export const metadata = {
+    title: "Promptopia",
+    description: "AI prompt share for programmers",
+
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang='en'>
+        <body>
+            <div className='main'>
+                <div className='gradient'/>
+            </div>
+
+            <main className="app">
+                <Nav />
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
