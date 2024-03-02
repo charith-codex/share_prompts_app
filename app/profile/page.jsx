929d681700c3defb69f6c5bc176d44a212582989
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-03-02 22:13:57 +0530

    version 1.6
    add prompt edit option

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+'use client'
 
-const Profile = () => {
+import {useState, useEffect} from 'react'
+import {useSession} from 'next-auth/react'
+import {useRouter} from 'next/navigation'
+
+import Profile from '@components/Profile'
+
+const MyProfile = () => {
+  const router = useRouter()
+  const {data: session} = useSession()
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    };
+
+   if(session?.user.id) fetchPosts();
+  }, []);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`)
+  }
+
+  const handleDelete = async (post) => {
+
+  }
+
   return (
-    <div>Profile</div>
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
   )
 }
 
-export default Profile
+export default MyProfile
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,18 +1,20 @@
-'use client';
+"use client";
 
 import { useState, useEffect } from "react";
 import PromptCard from "./PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
-  return (<div className="mt-16 prompt_layout">
-    {data.map((post) => (
-      <PromptCard
-        key={post._id}
-        post={post}
-        handleTagClick={handleTagClick}
-      />
-    ))}
-  </div>);
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
 };
 
 const Feed = () => {
