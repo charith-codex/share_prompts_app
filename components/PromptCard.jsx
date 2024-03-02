2cfd09309561d1559ed38c9be16638742b4021c4
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-03-02 22:42:04 +0530

    version 1.7
    fix some bugs

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -46,7 +46,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       </div>
 
       <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
-      <p className="font-inter text-sm blue_gradient cursor-pointer" onClick={() => handleTagClick && handleTagClick(post.tag)}>{post.tag}</p>
+      <p className="font-inter text-sm blue_gradient cursor-pointer" onClick={() => handleTagClick && handleTagClick(post.tag)}>#{post.tag}</p>
 
       {session?.user.id === post.creator._id && pathName === '/profile' && (
         <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
